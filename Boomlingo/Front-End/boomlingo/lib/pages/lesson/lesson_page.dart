cfb328dfa9de1499a8a3e1ec9c76dfe778727027
rbrunney <rbrunney@student.neumont.edu>
2023-02-09 11:24:23 -0700

    Added Text Font For App
    Added Specific Text Font for App

--- Boomlingo/Front-End/boomlingo/lib/pages/lesson/lesson_page.dart
+++ Boomlingo/Front-End/boomlingo/lib/pages/lesson/lesson_page.dart
@@ -16,9 +16,45 @@ class _LessonPageState extends State<LessonPage> {
           backgroundColor: const Color(global_style.pageBackgroundColor),
           body: SingleChildScrollView(
             child: Column(
-                children: const [
-                  Card(
-                    child: Icon(Icons.arrow_back),
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                        vertical: MediaQuery.of(context).size.height * 0.05,
+                        horizontal: MediaQuery.of(context).size.width * 0.05),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          height: 50,
+                          width: 50,
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: const Icon(Icons.arrow_back_rounded),
+                          ),
+                        ),
+                        const Spacer(),
+                        Text(
+                            'What The Dog Doin',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontFamily: global_style.textFont
+                          ),
+                        ),
+                        const Spacer(),
+                        SizedBox(
+                          height: 50,
+                          width: 50,
+                          child: Card(
+                            color: const Color(global_style.darkBlueAccentColor),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: const Icon(Icons.checklist_rounded, color: Color(global_style.whiteAccentColor),),
+                          ),
+                        ),
+                      ],
+                    )
                   )
                 ],
               ),
--- Boomlingo/Front-End/boomlingo/lib/util/style/global_style.dart
+++ Boomlingo/Front-End/boomlingo/lib/util/style/global_style.dart
@@ -1,7 +1,10 @@
 // Boomlingo Color Pallete
-const int darkBlueAccentColor = 0xFFF45562;
+const int darkBlueAccentColor = 0xFF1756FF;
 const int lightBlueAccentColor = 0xFFBACCFD;
 const int whiteAccentColor = 0xFFFFFFFF;
 const int pageBackgroundColor = 0xFFEDF2FF;
 const int starColor = 0xFFFFBB3A;
 const int textColor = 0xFF414872;
+
+// Boomlingo Text Fonts
+String textFont = "Inter";
