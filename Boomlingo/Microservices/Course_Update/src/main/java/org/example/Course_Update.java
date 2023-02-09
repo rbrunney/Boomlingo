138b6ef45c2a34fd3b7e8c18334062721107418a
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2023-02-09 10:53:11 -0700

    Made CRUD for Courses

---
+++ Boomlingo/Microservices/Course_Update/src/main/java/org/example/Course_Update.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Course_Update implements RequestHandler<Map<String, String>, String> {
+    @Override
+    public String handleRequest(Map<String, String> input, Context context) {
+        //Lessons are in a comma separated string
+        String title = input.get("Title");
+        ArrayList<String> lessonIds = new ArrayList<String>();
+        String lessons = input.get("Lessons");
+        String[] lessonsToSplit = lessons.split(",");
+        for (String lesson:lessonsToSplit) {
+            lessonIds.add(lesson);
+        }
+        //write object to DB
+
+        return "Course: " + title + " added.";
+    }
+}
