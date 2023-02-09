870f6d7dd481ef0f2549bfceb541fdb0563405ec
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2023-02-09 10:52:33 -0700

    Added WebRTC dependencies (Also added webrtc microservice - sorry Rob!)

---
+++ Boomlingo/Microservices/SpringWebRTC/target/classes/static/client.js
@@ -0,0 +1,81 @@
+var conn = new WebSocket('ws://localhost:8080/socket');
+
+function send(message) {
+    conn.send(JSON.stringify(message));
+}
+
+configuration = null;
+var peerConnection = new RTCPeerConnection(configuration);
+
+var dataChannel = peerConnection.createDataChannel("dataChannel", { reliable: true });
+
+dataChannel.onerror = function(error) {
+    console.log("Error:", error);
+};
+dataChannel.onclose = function() {
+    console.log("Data channel is closed");
+};
+
+peerConnection.createOffer(function(offer) {
+    send({
+        event : "offer",
+        data : offer
+    });
+    peerConnection.setLocalDescription(offer);
+}, function(error) {
+    // Handle error here
+});
+
+
+peerConnection.onicecandidate = function(event) {
+    if (event.candidate) {
+        send({
+            event : "candidate",
+            data : event.candidate
+        });
+    }
+};
+
+peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+
+peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+peerConnection.createAnswer(function(answer) {
+    peerConnection.setLocalDescription(answer);
+        send({
+            event : "answer",
+            data : answer
+        });
+}, function(error) {
+    // Handle error here
+});
+
+function handleAnswer(answer){
+    peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+}
+
+dataChannel.send("message");
+
+dataChannel.onmessage = function(event) {
+    console.log("Message:", event.data);
+};
+
+peerConnection.ondatachannel = function (event) {
+    dataChannel = event.channel;
+};
+
+const constraints = {
+    video: true,
+    audio : true,
+    "iceServers" : [ {
+        "url" : "stun:stun2.1.google.com:19302"
+    } ]
+};
+navigator.mediaDevices.getUserMedia(constraints).
+  then(function(stream) { /* use the stream */ })
+    .catch(function(err) { /* handle the error */ });
+
+peerConnection.addStream(stream);
+
+peerConnection.onaddstream = function(event){
+    videoElement.srcObject = event.stream;
+}
