cc81c242f5fcab878a636e7ed66e6cbf08918211
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2023-02-12 17:50:19 -0700

    More changes to webrtc related components Now basic video share is "implemented" needs fixing and revamping

--- Boomlingo/Front-End/boomlingo/lib/rtc_stateful_display.dart
+++ Boomlingo/Front-End/boomlingo/lib/rtc_stateful_display.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,9 +15,112 @@ class RTCVideoDisplay extends StatefulWidget {
 const title = "RTC_VIDEO_WINDOW";
 
 final _localVideoRenderer = RTCVideoRenderer();
+final _remoteVideoRenderer = RTCVideoRenderer();
+
+final sdpController = TextEditingController();
+bool _offer = false;
+
+RTCPeerConnection? _peerConnection;
 
 void initRenderers() async {
   await _localVideoRenderer.initialize();
+  await _createPeerConnection().then((value) {
+    print(value);
+    print(
+        "_printConnection is being set to value returned by _createPeerConnection()");
+    _peerConnection = value;
+  });
+}
+
+Future<RTCPeerConnection> _createPeerConnection() async {
+  Map<String, dynamic> configuration = {
+    "iceServers": [
+      {"url": "stun:stun.l.google.com:19302"},
+    ]
+  };
+
+  final Map<String, dynamic> offerSdpConstraints = {
+    "mandatory": {
+      "OfferToReceiveAudio": true,
+      "OfferToReceiveVideo": true,
+    },
+    "optional": [],
+  };
+
+  late MediaStream localStream;
+
+  localStream = await _getUserMedia();
+
+  RTCPeerConnection peerConnection =
+      await createPeerConnection(configuration, offerSdpConstraints);
+
+  peerConnection.addStream(localStream);
+
+  peerConnection.onIceCandidate = (e) {
+    if (e.candidate != null) {
+      print(json.encode({
+        'candidate': e.candidate,
+        'sdpMid': e.sdpMid,
+        'sdpMlineIndex': e.sdpMLineIndex
+      }));
+    }
+  };
+
+  peerConnection.onIceConnectionState = (e) {
+    print(e);
+  };
+
+  peerConnection.onAddStream = (stream) {
+    print("addStream: " + stream.id);
+    _remoteVideoRenderer.srcObject = stream;
+  };
+
+  print("resulting peer connection object");
+  print(peerConnection);
+
+  return peerConnection;
+}
+
+void _createOffer() async {
+  RTCSessionDescription description =
+      await _peerConnection!.createOffer({'offerToReceiveVideo': 1});
+  var session = description.sdp.toString();
+  print(json.encode(session));
+  _offer = true;
+
+  _peerConnection!.setLocalDescription(description);
+}
+
+void _createAnswer() async {
+  RTCSessionDescription description =
+      await _peerConnection!.createAnswer({'offerToReceiveVideo': 1});
+
+  var session = json.decode(description.sdp.toString());
+  print(json.encode(session));
+
+  _peerConnection!.setLocalDescription(description);
+}
+
+void _setRemoteDescription() async {
+  String jsonString = sdpController.text;
+  dynamic session = await jsonDecode(jsonString);
+
+  String sdp = json.encode(session);
+
+  RTCSessionDescription description =
+      RTCSessionDescription(sdp, _offer ? 'answer' : 'offer');
+  print(description.toMap());
+
+  await _peerConnection!.setRemoteDescription(description);
+}
+
+void _addCandidate() async {
+  String jsonString = sdpController.text;
+  dynamic session = await jsonDecode(jsonString);
+  print(session['candidate']);
+  dynamic candidate = RTCIceCandidate(
+      session['candidate'], session['sdpMid'], session['sdpMlineIndex']);
+  await _peerConnection!.addCandidate(candidate);
 }
 
 _getUserMedia() async {
@@ -29,6 +134,7 @@ _getUserMedia() async {
   MediaStream stream =
       await navigator.mediaDevices.getUserMedia(mediaConstraints);
   _localVideoRenderer.srcObject = stream;
+  return stream;
 }
 
 class _RTCVideoDisplayState extends State<RTCVideoDisplay> {
@@ -36,14 +142,38 @@ class _RTCVideoDisplayState extends State<RTCVideoDisplay> {
   void initState() {
     initRenderers();
     _getUserMedia();
+
     super.initState();
   }
 
   @override
   void dispose() async {
     await _localVideoRenderer.dispose();
+    sdpController.dispose();
     super.dispose();
   }
+
+  SizedBox videoRenderers() => SizedBox(
+        height: 210,
+        child: Row(
+          children: [
+            Flexible(
+                child: Container(
+              key: Key('local'),
+              margin: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+              decoration: BoxDecoration(color: Colors.black),
+              child: RTCVideoView(_localVideoRenderer),
+            )),
+            Flexible(
+                child: Container(
+              key: Key("remote"),
+              margin: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+              decoration: BoxDecoration(color: Colors.black),
+              child: RTCVideoView(_remoteVideoRenderer),
+            ))
+          ],
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -52,14 +182,53 @@ class _RTCVideoDisplayState extends State<RTCVideoDisplay> {
         // ignore: prefer_const_constructors
         title: Text(title),
       ),
-      body: Stack(
+      body: Column(
         children: [
-          Positioned(
-              top: 0.0,
-              right: 0.0,
-              left: 0.0,
-              bottom: 0.0,
-              child: RTCVideoView(_localVideoRenderer))
+          videoRenderers(),
+          Row(children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: TextField(
+                  controller: sdpController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 4,
+                  maxLength: TextField.noMaxLength,
+                ),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                ElevatedButton(
+                  onPressed: _createOffer,
+                  child: Text("Offer"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  onPressed: _createAnswer,
+                  child: Text("Answer"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  onPressed: _setRemoteDescription,
+                  child: Text("Set Remote Description"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  onPressed: _addCandidate,
+                  child: Text("Set Candidate"),
+                ),
+              ],
+            )
+          ]),
         ],
       ),
     );
