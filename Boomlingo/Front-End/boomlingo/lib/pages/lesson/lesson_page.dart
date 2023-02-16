e7903508d86dcdd7776206d59f01ff49a9dd8d36
rbrunney <rbrunney@student.neumont.edu>
2023-02-16 11:54:04 -0700

    Adding Functionality to VideoPlayer
    Making Icons into buttons, so it has functionality with the VideoPlayer Card, just have to wrap up a few more and then it is all good!

--- Boomlingo/Front-End/boomlingo/lib/pages/lesson/lesson_page.dart
+++ Boomlingo/Front-End/boomlingo/lib/pages/lesson/lesson_page.dart
@@ -11,22 +11,24 @@ class LessonPage extends StatefulWidget {
 }
 
 class _LessonPageState extends State<LessonPage> {
+  bool isVideoPlaying = false;
+  bool isVideoMute = false;
 
   final YoutubePlayerController _controller = YoutubePlayerController(
-      initialVideoId: 'gPPxfPThq20',
-      flags: const YoutubePlayerFlags(
-          autoPlay: false,
-          mute: true,
-      ),
+    initialVideoId: 'gPPxfPThq20',
+    flags: const YoutubePlayerFlags(
+      autoPlay: false,
+      mute: false,
+    ),
   );
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          backgroundColor: const Color(global_style.pageBackgroundColor),
-          body: SingleChildScrollView(
-            child: Column(
+            backgroundColor: const Color(global_style.pageBackgroundColor),
+            body: SingleChildScrollView(
+              child: Column(
                 children: [
                   Container(
                     margin: EdgeInsets.symmetric(
@@ -46,84 +48,117 @@ class _LessonPageState extends State<LessonPage> {
                         ),
                         const Spacer(),
                         Text(
-                            'What The Dog Doin',
+                          'What The Dog Doin',
                           style: TextStyle(
-                            fontSize: 18,
-                            fontFamily: global_style.textFont,
-                            fontWeight: FontWeight.bold
-                          ),
+                              fontSize: 18,
+                              fontFamily: global_style.textFont,
+                              fontWeight: FontWeight.bold),
                         ),
                         const Spacer(),
                         SizedBox(
                           height: 50,
                           width: 50,
                           child: Card(
-                            color: const Color(global_style.darkBlueAccentColor),
+                            color:
+                                const Color(global_style.darkBlueAccentColor),
                             shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10),
                             ),
-                            child: const Icon(Icons.checklist_rounded, color: Color(global_style.whiteAccentColor),),
+                            child: const Icon(
+                              Icons.checklist_rounded,
+                              color: Color(global_style.whiteAccentColor),
+                            ),
                           ),
                         ),
                       ],
                     ),
                   ),
                   SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.35,
-                    width: MediaQuery.of(context).size.width * 0.90,
-                    child: Card(
-                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
-                      child: Column(
-                        children: [
-                          ClipRRect(
-                            borderRadius: const BorderRadius.only(topRight: Radius.circular(15), topLeft: Radius.circular(15)), 
-                            child: YoutubePlayer(
-                              controller: _controller,
-                              showVideoProgressIndicator: true
-                            ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-                            child: Row(
-                              children: const [
-                                Icon(Ionicons.volume_high_outline),
-                                Spacer(),
-                                Icon(Ionicons.md_play_skip_back_outline),
-                                Spacer(),
-                                Icon(Ionicons.pause_outline),
-                                Spacer(),
-                                Icon(Ionicons.md_play_skip_forward_outline),
-                                Spacer(),
-                                Icon(MaterialCommunityIcons.arrow_expand)
-                              ],
-                            )
-                          )
-                        ],
-                      )
-                    )
-                  ),
-                  SizedBox(
-                    child: Container(
-                      margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.05, vertical: 20),
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      width: MediaQuery.of(context).size.width * 0.90,
                       child: Card(
-                        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
-                        child: Row(
-                          children: const [
-                            CircleAvatar(
-                              radius: 30,
-                              backgroundColor: Colors.black,
-                            ),
-                            Spacer(),
-                            Icon(Ionicons.md_chevron_up_outline)
-                          ]
-                        )
-                      )
-                    )
-                  )
+                          shape: const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          child: Column(
+                            children: [
+                              ClipRRect(
+                                borderRadius: const BorderRadius.only(
+                                    topRight: Radius.circular(15),
+                                    topLeft: Radius.circular(15)),
+                                child: YoutubePlayer(
+                                    controller: _controller,
+                                    showVideoProgressIndicator: true),
+                              ),
+                              Container(
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 20, vertical: 12),
+                                  child: Row(
+                                    children: [
+                                      IconButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              isVideoMute = !isVideoMute;
+
+                                              if (isVideoMute) {
+                                                _controller.mute();
+                                              } else {
+                                                _controller.unMute();  
+                                              }
+                                            });
+                                          },
+                                          icon: Icon(isVideoMute
+                                              ? Ionicons.volume_mute_outline
+                                              : Ionicons.volume_high_outline)),
+                                      const Spacer(),
+                                      const Icon(
+                                          Ionicons.md_play_skip_back_outline),
+                                      const Spacer(),
+                                      IconButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              isVideoPlaying = !isVideoPlaying;
+
+                                              if (isVideoPlaying) {
+                                                _controller.play();
+                                              } else {
+                                                _controller.pause();  
+                                              }
+                                            });
+                                          },
+                                          icon: Icon(isVideoPlaying
+                                              ? Ionicons.pause_outline
+                                              : Ionicons.play_outline)),
+                                      const Spacer(),
+                                      const Icon(Ionicons
+                                          .md_play_skip_forward_outline),
+                                      const Spacer(),
+                                      const Icon(
+                                          MaterialCommunityIcons.arrow_expand)
+                                    ],
+                                  ))
+                            ],
+                          ))),
+                  SizedBox(
+                      child: Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.05,
+                              vertical: 20),
+                          child: Card(
+                              shape: const RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15))),
+                              child: Row(children: const [
+                                CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.black,
+                                ),
+                                Spacer(),
+                                Icon(Ionicons.md_chevron_up_outline)
+                              ]))))
                 ],
               ),
-            )
-          )
-    );
+            )));
   }
 }
