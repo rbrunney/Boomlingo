ec39c01cc61fc34850179d4caadb23be2a437ff8
rbrunney <rbrunney@student.neumont.edu>
2023-02-21 15:59:55 -0700

    Updated Expandable Cards
    Updated Expandable Cards for Lesson Page

--- Boomlingo/Front-End/boomlingo/lib/pages/lesson/expandable_card.dart
+++ Boomlingo/Front-End/boomlingo/lib/pages/lesson/expandable_card.dart
@@ -4,47 +4,53 @@ import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class ExpandableCard extends StatefulWidget {
   final String cardHeader;
-  const ExpandableCard({
-    super.key,
-    this.cardHeader = ''
-    });
+  const ExpandableCard({super.key, this.cardHeader = ''});
 
   @override
   State<ExpandableCard> createState() => _ExpandableCardState();
 }
 
 class _ExpandableCardState extends State<ExpandableCard> {
+  bool isExpanded = false;
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        child: Container(
-            margin: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.05,
-                vertical: 20),
-            child: Card(
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15))),
-                child: Container(
-                    padding: const EdgeInsets.only(
-                        left: 12, right: 20, top: 10, bottom: 10),
-                    child: Row(children: [
-                      const CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Colors.black,
-                      ),
-                      const Spacer(),
-                      Container(
-                        alignment: Alignment.center,
-                        child: Text(
-                          widget.cardHeader,
-                          style: TextStyle(
-                              fontSize: 15,
-                              fontFamily: global_style.textFont,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      const Spacer(),
-                      const Icon(Ionicons.md_chevron_down)
-                    ])))));
+    return InkWell(
+        onTap: () {
+          setState(() {
+            isExpanded = !isExpanded;
+          });
+        },
+        child: SizedBox(
+            child: Container(
+                margin: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * 0.05,
+                    vertical: 20),
+                child: Card(
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(15))),
+                    color: isExpanded ? const Color(global_style.darkBlueAccentColor) :const Color(global_style.whiteAccentColor),
+                    child: Container(
+                        padding: const EdgeInsets.only(
+                            left: 12, right: 20, top: 10, bottom: 10),
+                        child: Row(children: [
+                          Container(
+                            alignment: Alignment.center,
+                            child: Text(
+                              widget.cardHeader,
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontFamily: global_style.textFont,
+                                  fontWeight: FontWeight.bold,
+                                  color: isExpanded ? const Color(global_style.whiteAccentColor) : const Color(global_style.textColor)
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          Icon(
+                            isExpanded ? Ionicons.md_chevron_up : Ionicons.md_chevron_down,
+                            color: isExpanded ? const Color(global_style.whiteAccentColor) : const Color(global_style.textColor)
+                          )
+                        ]))))));
   }
 }
