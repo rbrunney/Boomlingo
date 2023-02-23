5399e2b1289dd771248a7702fd1ea34ed5fe78bd
joshuaj25679 <79765022+joshuaj25679@users.noreply.github.com>
2023-02-23 10:24:06 -0700

    ....

---
+++ Boomlingo/Microservices/User_Create/user_create.py
@@ -0,0 +1,32 @@
+import mysql.connector
+import json
+
+def lambda_handler(event, context):
+    mydb = mysql.connector.connect(
+        host="boomlingo-db.cf61ggafcjsa.us-west-2.rds.amazonaws.com",
+        user="admin",
+        password="MyBedRightNow",
+        database="boomlingo"
+    )
+    
+    mycursor = mydb.cursor()
+
+    sql = "INSERT INTO users (username, password, email, firstname, lastname, DOB, joindate) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    val = (event["username"], event["password"], event["email"], event["firstname"], event["lastname"], event["DOB"], event["joindate"])
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+    print(mycursor.rowcount, "record inserted.")
+
+test_vals = {
+    "username":"YourMom",
+    "password":"myBed",
+    "email":"yourmominmybed@rightnow.com",
+    "firstname":"My",
+    "lastname":"Mom",
+    "DOB":"12/12/1972",
+    "joindate":"2/22/2023"
+    }
+
+
+lambda_handler(test_vals, "None")
