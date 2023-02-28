bc11bf631e3fadd09d2589346044aa411a869611
DNgo-Neumont <78094165+DNgo-Neumont@users.noreply.github.com>
2023-02-28 10:47:29 -0700

    Changes to socket server and signaling

--- Boomlingo/Front-End/boomlingo/lib/rtc_stateful_display.dart
+++ Boomlingo/Front-End/boomlingo/lib/rtc_stateful_display.dart
@@ -16,7 +16,6 @@ class RTCVideoDisplay extends StatefulWidget {
 class _RTCVideoDisplayState extends State<RTCVideoDisplay> {
   RTCVideoRenderer _localRenderer = RTCVideoRenderer();
   RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
-  String? roomID;
   TextEditingController textEditingController = TextEditingController();
   Signaling signaler = Signaling();
   @override
@@ -25,7 +24,11 @@ class _RTCVideoDisplayState extends State<RTCVideoDisplay> {
     _remoteRenderer.initialize();
     // signaler.createRoom(_remoteRenderer);
     signaler.initChannel();
-    signaler.createRoom(_remoteRenderer);
+    signaler.onAddRemoteStream = ((stream) {
+      _remoteRenderer.srcObject = stream;
+      setState(() {});
+    });
+    // signaler.initRTC(_remoteRenderer);
     super.initState();
   }
 
@@ -39,11 +42,87 @@ class _RTCVideoDisplayState extends State<RTCVideoDisplay> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // ignore: prefer_const_constructors
-        title: Text("bruh"),
+      // appBar: AppBar(
+      //   // ignore: prefer_const_constructors
+      //   title: Text("bruh"),
+      // ),
+      // body: Column(),
+      body: Column(
+        children: [
+          SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  signaler.openUserMedia(_localRenderer, _remoteRenderer);
+                },
+                child: Text("Open camera & microphone"),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  // signaler.initChannel();
+                  signaler.createRoom(_remoteRenderer);
+                },
+                child: Text("Create room"),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // Add roomId
+                  signaler.joinRoom(
+                    _remoteRenderer,
+                    // textEditingController.text,
+                  );
+                },
+                child: Text("Join room"),
+              ),
+              SizedBox(
+                width: 8,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  signaler.hangUp(_localRenderer);
+                },
+                child: Text("Hangup"),
+              )
+            ],
+          ),
+          SizedBox(height: 8),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: RTCVideoView(_localRenderer, mirror: true)),
+                  Expanded(child: RTCVideoView(_remoteRenderer)),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Join the following Room: "),
+                Flexible(
+                  child: TextFormField(
+                    controller: textEditingController,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 8)
+        ],
       ),
-      body: Column(),
     );
   }
 }
