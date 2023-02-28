314093e3769dac3bd8604395c4258f1e8032a4df
Scipion2002 <79728249+Scipion2002@users.noreply.github.com>
2023-02-28 11:59:29 -0700

    Search user for video Call Updated
    All I do is search for a user and I will change the icon so it goes to the call edit

--- Boomlingo/Front-End/boomlingo/lib/pages/video_call/video_call_page.dart
+++ Boomlingo/Front-End/boomlingo/lib/pages/video_call/video_call_page.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:collection';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'add_friend_card.dart';
 
 class VideoCallPage extends StatefulWidget {
   const VideoCallPage({super.key});
@@ -8,9 +12,80 @@ class VideoCallPage extends StatefulWidget {
   State<VideoCallPage> createState() => _VideoCallPageState();
 }
 
-class _VideoCallPageState extends State<VideoCallPage> {
+class _VideoCallPageState extends State<VideoCallPage>
+    with SingleTickerProviderStateMixin {
+  List<Widget> userSearch = [];
+  Future<String>? futureSearchInfo;
+  bool _isActive = false;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    TextEditingController _searchController = TextEditingController();
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+          title: Row(
+        children: [
+          if (!_isActive)
+            Text("Search User",
+                style: Theme.of(context).appBarTheme.titleTextStyle),
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: AnimatedSize(
+                duration: const Duration(milliseconds: 250),
+                child: _isActive
+                    ? Container(
+                        width: double.infinity,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            color: Colors.white12,
+                            borderRadius: BorderRadius.circular(4.0)),
+                        child: TextField(
+                          controller: _searchController,
+                          decoration: InputDecoration(
+                              hintText: 'Search for something',
+                              prefixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    userSearch.clear();
+                                  });
+                                  userSearch.add(AddFriendCard(
+                                    userId: 1,
+                                    username: "Alex Turro",
+                                  ));
+                                },
+                                icon: const Icon(Icons.search),
+                              ),
+                              suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isActive = false;
+                                    });
+                                  },
+                                  icon: const Icon(Icons.close))),
+                        ),
+                      )
+                    : IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _isActive = true;
+                          });
+                        },
+                        icon: const Icon(Icons.search)),
+              ),
+            ),
+          ),
+        ],
+      )),
+      body: SingleChildScrollView(
+          child: Column(
+        children: userSearch,
+      )),
+    ));
+  }
+
+  void showData(filter, value) {
+    LinkedHashMap<String, dynamic> data = json.decode(value);
   }
 }
